8e96798b0071ec0bb5a065ca9aac91f4d8cc8b59
Chitakov <chitakov2606nikita@gmail.com>
2022-06-09 18:45:03 +0300

    Add project files.

---
+++ Lab3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = 1, f;
+            try
+            {
+            Console.WriteLine("Введите -3<=x<=5");
+            double x = Convert.ToDouble(Console.ReadLine());
+            if (x >= -3 && x <= -2) f = - x - 2;
+            else if (x > -2 && x <= -1) f = Math.Sqrt(Math.Pow(R, 2) - Math.Pow(x + 1, 2));
+            else if (x > -1 && x <= 1) f = 1;
+            else if (x > 1 && x <= 2) f = - 2 * x + 3;
+            else f = -1;
+            Console.WriteLine("Для x=" + x);
+            Console.WriteLine("f={0}", f);
+            Console.ReadLine();
+            }
+            catch (FormatException)
+            { Console.WriteLine("Неверный ввод");
+            Console.ReadLine();
+            }
+        }
+    }
+
+}
---
+++ Lab3_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите точность");
+            double ep = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите начало отрезка");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите конец отрезка");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double x = a, s, y;
+            const int MaxIter = 500;
+            bool done = true;
+            int i;
+            int j = 0;
+            while (h * Math.Pow(10, 1 + j) % 10 != 0) { j++; }
+            Console.WriteLine(" __________________________________________________");
+            Console.WriteLine("|  x  |    Сумма ряда    |  n  |    Зн. функции   |");
+            Console.WriteLine(" __________________________________________________");
+            while (x <= b)
+            {
+                s = x; y = s;
+                done = true;
+                for (i = 1; Math.Abs(s) > ep; i++)
+                {
+                    s *= Math.Pow(x, 2) * (2 * i - 1) / (2 * i + 1);
+                    y += s;
+                    if (i > MaxIter) { done = false; break; }
+                }
+                if (done == true) Console.WriteLine("|{0,5}|{1,18}|{2,5}|{3,18}| ", x, y, i, Math.Log((1 + x) / (1 - x)) / 2);// Math.Atan(x));
+                else Console.WriteLine("|{0,5}|  Ряд расходится  |     |                  ", x);
+                x += h;
+                x = Math.Round(x, j);
+            }
+            Console.WriteLine(" __________________________________________________");
+            Console.ReadLine();
+        }
+    }
+}
